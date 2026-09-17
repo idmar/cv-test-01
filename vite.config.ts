ca90bf1d3d66ff3278d4e6bbd22d6679ddac26f0
idmar <idmar@users.noreply.github.com>
2026-09-17 11:32:43 +0800

    增加 Action 发布脚本

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
+  const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1];
+
   return {
+    base: process.env.GITHUB_ACTIONS && repositoryName ? `/${repositoryName}/` : '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
